062d27be065963452d7fb37f0f05db875deb89d8
Diego <amozaka06@outlook.com>
2025-04-19 00:37:05 +0200

    Updated

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+author 'Diego'
+description 'Garage System for Gangs [QBCore] with Plates options'
+client_scripts { 'main.lua' }
+shared_scripts { 'shared.lua' }
+dependencies {
+    'qb-core',
+    'qb-menu',
+    'qb-target',
+}
+
+
+
+
---
+++ shared.lua
@@ -0,0 +1,14 @@
+Config = Config or {}
+
+Config.Gangs = {
+    ["vagos"] = {
+        ["VehicleSpawner"] = vector4(0.0, 0.0, 0.0, 0.0), 
+        ["colors"] = { 13, 17 },
+        ["PLATE"] = "VAGOS",
+        ["vehicles"] = {
+            ["faggio"] = "Faggio",
+            ["vamos"] = "Vamos",
+            ["buffalo4"] = "Buffalo STX",
+        },
+    },
+}
